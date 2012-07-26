2be8c69e6978990bc342fde0efdc8bdb031702eb
Angus Hammond <angusgh@gmail.com>
2012-07-26 14:26:32 +0100

    Added initial vm tests

--- vm/test/vm-test.h
+++ vm/test/vm-test.h
@@ -3,10 +3,38 @@
 
 class VMTests: public CxxTest::TestSuite
 {
-	public:
-	void test_tests(void)
+	VM* vm;
+
+	void setUp(void)
 	{
-		TS_ASSERT(true);
+		vm=new VM("");
+	}
+
+	void tearDown(void)
+	{
+		delete vm;
+	}
+
+	public:
+	void test_nop(void)
+	{
+		vm->nop();
+		TS_ASSERT_EQUALS(vm->r1, 0);
+		TS_ASSERT_EQUALS(vm->r2, 0);
+		TS_ASSERT_EQUALS(vm->r3, 0);
+		TS_ASSERT_EQUALS(vm->r4, 0);
+		TS_ASSERT_EQUALS(vm->r5, 0);
+		TS_ASSERT_EQUALS(vm->ip, 0);
+	}
+
+	void test_jmp(void)
+	{
+		vm->r2=5;
+		vm->jmp();
+		TS_ASSERT_EQUALS(vm->ip, 5);
+		vm->r2=10;
+		vm->jmp();
+		TS_ASSERT_EQUALS(vm->ip, 10);
 	}
 
 };
