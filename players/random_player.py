e4722afcb1a2f66630e904374c3ed79dc608175e
Angus Hammond <angusgh@gmail.com>
2012-08-04 23:41:08 +0100

    Remove debuging prints from random_player.py

--- players/random_player.py
+++ players/random_player.py
@@ -4,8 +4,6 @@ try:
     while True:
         board=raw_input()
         x=random.randrange(64)
-        print x
-        print board[x]
         while board[x]!="0":
             x=random.randrange(64)
         print x
