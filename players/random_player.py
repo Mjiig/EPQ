ef45c643aff37c1ed8a1a13493d4fbee65b60939
Angus Hammond <angusgh@gmail.com>
2012-08-21 23:11:29 +0100

    Make random_player.py use shorter board length

--- players/random_player.py
+++ players/random_player.py
@@ -6,9 +6,9 @@ random.seed("This kind of phrase is what happens when I have to come up with a s
 try:
     while True:
         board=raw_input()
-        x=random.randrange(64)
+        x=random.randrange(9)
         while board[x]!="0":
-            x=random.randrange(64)
+            x=random.randrange(9)
         print x
 except EOFError:
     pass
